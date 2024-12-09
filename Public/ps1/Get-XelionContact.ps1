b183820d827179903e3b7ef6eafc13fb1ef42cf1
Lorentz <84975855+Lorentz86@users.noreply.github.com>
2024-12-09 16:36:22 +0100

    Making a custom list for easy exporting to csv files

--- Public/ps1/Get-XelionContact.ps1
+++ Public/ps1/Get-XelionContact.ps1
@@ -6,19 +6,59 @@ function Get-XelionContact{
 
         [Parameter(Mandatory=$false, HelpMessage="Sort by name or mru(Most Recent Used,Default). If mru is used include fields can only be status and employment.")]
         [ValidateSet("Name","mru")]
-        [string]$SortBy='mru'
+        [string]$SortBy='mru',
+
+        [Parameter(Mandatory=$false, HelpMessage="Raw Contact information so you can make your own filter")]
+        [switch]$Raw
     )
 
     try {
-        $AllContacts = Get-XelionAddressables -name $Name -SortBy $SortBy
+            if($Raw.IsPresent){
+                $AllContacts = Get-XelionAddressables -name $Name -SortBy $SortBy
         
-        $ContactList = [System.Collections.ArrayList]::new()
-        foreach($Contact in $AllContacts){
-                Write-Information -MessageData "Current Contact: $($Contact.commonName) `nCurrent OID: $($Contact.oid)"
-                $Info = Get-XelionAddressables -oid $contact.oid
-                $ContactList.Add($Info) | Out-Null
+                $ContactList = [System.Collections.ArrayList]::new()
+                foreach($Contact in $AllContacts){
+                        Write-Information -MessageData "Current Contact: $($Contact.commonName) `nCurrent OID: $($Contact.oid)"
+                        $Info = Get-XelionAddressables -oid $contact.oid
+                        $ContactList.Add($Info) | Out-Null
+                    }
+                    return $ContactList
             }
-            return $ContactList
+            else{
+                $AllContacts = Get-XelionAddressables -name $Name -SortBy $SortBy
+                $ContactList = [System.Collections.ArrayList]::new()
+                
+                Foreach($contact in $AllContacts) {
+                    $Info = Get-XelionAddressables -oid $contact.oid
+
+                    $contactInfo = [PSCustomObject]@{
+                    displayName = $Info.commonName
+                    givenName = $Info.givenName
+                    lastname = $Info.lastname
+                    objectType = $Info.objectType
+                    }
+
+                    $privateEmailCount = 1
+                    $privateNumberCount = 1
+                    foreach ($privateInformation in $Info.telecomAddresses) {                        
+                        if($privateInformation.addressType -match "Email"){
+                            Write-Host $privateInformation.address
+                            $privateEmailHeader = "PrivateEmail" + $privateEmailCount
+                            $contactInfo | Add-Member -MemberType NoteProperty -Name $privateEmailHeader -Value $privateInformation.address
+                            $privateEmailCount++
+                        }
+
+                        if($privateInformation.addressType -match "Telephone"){
+                            $privatePhoneHeader = "PrivatePhone" + $privateNumberCount
+                            $contactInfo | Add-Member -MemberType NoteProperty -Name $privatePhoneHeader -Value $privateInformation.address
+                            $privateNumberCount++
+                        }
+                    }
+                    $ContactList.Add($contactInfo) | Out-Null
+                }
+                return $ContactList
+            }
+
         }
     catch {
         Write-Error "Could not get contact information: $_"
